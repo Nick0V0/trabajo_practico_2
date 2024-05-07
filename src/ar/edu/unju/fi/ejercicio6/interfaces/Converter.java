54e84451729afcf608ef8ac5069b30a94f494ccc
vilte <nicolasvilte08@gmail.com>
2024-05-06 23:06:05 -0300

    6. Uso de @FunctionalInterface.

---
+++ src/ar/edu/unju/fi/ejercicio6/interfaces/Converter.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio6.interfaces;
+
+public interface Converter<T,T1> {
+	T1 convert(T t);
+	
+	static <T> boolean isNotNull(T t) {
+		return t != null;
+	}
+	default void mostrarObjetos(T1 t1) {
+		System.out.println("Objeto - "+t1.toString());
+	}
+}
